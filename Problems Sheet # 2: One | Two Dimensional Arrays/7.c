336564b11e666d856e0f07db5afe10787ba1833c
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-13 11:21:09 +0300

    Create 7.c

---
+++ Problems Sheet # 2: One | Two Dimensional Arrays/7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,j;
+    for(i=0;i<5;i++) {
+        for(j=0;j<5;j++) {
+            if(i==j) printf("0 ");
+            else if(i>j) printf("-1 ");
+            else printf("1 ");
+        }
+        printf("\n");
+    }
+  
+    return 0;
+}
