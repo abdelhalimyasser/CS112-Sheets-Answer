519d558382074e5383ceb2d46b8097383d3576de
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-13 10:57:27 +0300

    Create 4.c

---
+++ Problems Sheet # 1: Functions (Recursion)/4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function prototype to declare it
+int sumNatural(int n)
+
+// Main function to test
+int main() 
+{
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    printf("Sum of first %d natural numbers = %d\n", n, sumNatural(n));
+    return 0;
+}
+
+// Recursive function to calculate sum of natural numbers
+int sumNatural(int n) {
+    if (n == 0)  
+        return 0;
+    return n + sumNatural(n - 1);
+}
