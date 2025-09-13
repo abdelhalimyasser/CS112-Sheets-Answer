823e5855b03a00d40bd976176eac0d6cac0c05b2
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-13 11:52:05 +0300

    Create 2.c

---
+++ Problems Sheet # 7: Search & Sort Algorithms/2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int swapped = 0;
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) 
+          break; 
+    }
+}
+
+int main() {
+    int arr[] = {5, 1, 4, 2, 8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    bubbleSort(arr, size);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
