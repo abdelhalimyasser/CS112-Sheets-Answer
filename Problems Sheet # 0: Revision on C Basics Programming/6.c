b31535d6099dd99b12c82b7fccf2ca889117c183
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-09-13 10:24:24 +0300

    Create 6.c

---
+++ Problems Sheet # 0: Revision on C Basics Programming/6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter 3 integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c)
+        printf("Greatest = %d\n", a);
+    else if (b >= a && b >= c)
+        printf("Greatest = %d\n", b);
+    else
+        printf("Greatest = %d\n", c);
+
+    return 0;
+}
